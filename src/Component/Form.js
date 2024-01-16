b2621e6f12160b90fb1a2760148bd9ce0051d96a
navgurukul <laxmiyadav21@navgurukul.org>
2024-01-16 22:51:29 +0530

    creating UI

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,12 +1,30 @@
-import { Box,Button,InputBase } from "@mui/material"
+import { Box,Button,InputBase,styled } from "@mui/material"
+const Container=styled(Box)({
+    background:"#445A6F",
+    padding:10
+})
+const Input=styled(InputBase)({
+    color:"#FFF",
+    marginRight:20,
+    fontSize:15
+})
+const GetButton=styled(Button)({
+    background:"#e67e22",
+    
+
+})
 const Form=()=>{
     return(
         <>
-        <Box>
-            <InputBase/>
-            <InputBase/>
-            <Button>Get whether</Button>
-        </Box>
+        <Container>
+            <Input
+            placeholder="City"
+            />
+            <Input
+            placeholder="Country"
+            />
+            <GetButton variant="contained">Get whether</GetButton>
+        </Container>
         </>
     )
 }
