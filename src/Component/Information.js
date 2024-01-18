9d39e377097283783f857433938a7dacf2ad76aa
navgurukul <laxmiyadav21@navgurukul.org>
2024-01-18 15:18:33 +0530

    weather aap

--- src/Component/Information.js
+++ src/Component/Information.js
@@ -1,19 +1,34 @@
-import { Typography, Box } from "@mui/material";
-// import LocationOnIcon from '@mui/icons-material/LocationOn';
-import {LocationOn,SettingsBrightness,Opacity,Brightness5,Brightness6,Dehaze} from "@mui/icons-material"
+import { Typography, Box,styled } from "@mui/material";
+import {Cloud,LocationOn,SettingsBrightness,Opacity,Brightness5,Brightness6,Dehaze} from "@mui/icons-material"
+const Row=styled(Typography)({
+  padding:10,
+  fontSize:20,
+  letterSpacing:2,
+  '&>svg':{
+    marginRight:10
+  }
+
+});
+const Error=styled(Typography)({
+  color:"red",
+  margin:50,
+  padding:20
+
+})
 const Information = ({ result }) => {
   return (
     result && Object.keys(result).length > 0 ? (
-      <Box>
-        <Typography variant="h5"><LocationOn/>Location:{result.name},
-        {result.sys.country}</Typography>
-        <Typography><SettingsBrightness/>Temperature:{result.main.temp}</Typography>
-        <Typography><Opacity/>Humidity:{result.main.humidity}</Typography>
-        <Typography><Brightness5/>Sun Rise:{result.sys.sunrise}</Typography>
-        <Typography><Brightness6/>Sun Set:{result.sys.sunset}</Typography>
-        <Typography><Dehaze/>Humidity:{result.weather[0].main}</Typography>
+      <Box style={{margin:"30px 60px"}}>
+        <Row variant="h5"><LocationOn/>Location:{result.name},
+        {result.sys.country}</Row>
+        <Row><SettingsBrightness/>Temperature:{result.main.temp}</Row>
+        <Row><Opacity/>Humidity:{result.main.humidity}</Row>
+        <Row><Brightness5/>Sun Rise:{new Date (result.sys.sunrise*1000).toLocaleTimeString()}</Row>
+        <Row><Brightness6/>Sun Set:{new Date(result.sys.sunset*1000).toLocaleTimeString()}</Row>
+        <Row><Dehaze/>Humidity:{result.weather[0].main}</Row>
+        <Row><Cloud/>Clouds:{result.clouds.all}%</Row>
       </Box>
-    ) : null
+    ) : <Error>please enter the value to check weather</Error>
   );
 };
 
